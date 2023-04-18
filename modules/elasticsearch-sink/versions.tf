401a7bedc17c0dfdfa8a89bf254b0868e0de4be3
Upendra Vedullapalli <upendra.rao.vedullapalli@entur.org>
2023-04-18 17:29:53 +0200

    ETU-36970: Update to import latest init module changes

--- modules/elasticsearch-sink/versions.tf
+++ modules/elasticsearch-sink/versions.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     aiven = {
-      source = "aiven/aiven"
+      source  = "aiven/aiven"
+      version = ">= 3.0.0, < 4.0.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">=4.40.0"
     }
   }
   required_version = ">=0.13.2"
